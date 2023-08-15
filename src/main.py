f835b247e707024b5e5db2f5bfc523868940f812
Miłosz Thiede <hello@majlitech.pl>
2023-08-15 16:52:43 +0200

    remove redirect to index.html, just return the file

--- src/main.py
+++ src/main.py
@@ -4,36 +4,44 @@ import random
 import importlib
 from bs4 import BeautifulSoup
 import traceback
-app = flask.Flask(__name__,static_url_path="/")
-app.static_folder="static/"
+app = flask.Flask(__name__, static_url_path="/")
+app.static_folder = "static/"
+
 
 @app.route("/")
 def indexredir():
-    return flask.redirect("/index.html")
+    with open("static/index.html") as f:
+        return f.read()
+
 
 @app.route("/basic/")
 def basicredir():
     return flask.redirect("/basic/omnie")
+
+
 @app.route("/basic/<site_url>")
 def basic(site_url):
     try:
         with open("static/content/"+site_url+".html") as site:
             content = site.read()
-            if content.find("<!-- allowsend -->")==-1:
+            if content.find("<!-- allowsend -->") == -1:
                 return flask.Response(status=403)
             soup = BeautifulSoup(content, 'html.parser')
             title_tag = soup.title
             if title_tag:
-                     title_tag.text.strip()
-            if content.find("<!-- template -->")!=-1:
-                templaterenderer = importlib.import_module("templates."+str(site_url)+".render")
-                return flask.render_template("basic.html",content=flask.Markup(templaterenderer.return_template()),title=title_tag.text.strip())
-            return flask.render_template("basic.html",title=title_tag.text.strip(),content=flask.Markup(content))
-    except Exception as e: 
+                title_tag.text.strip()
+            if content.find("<!-- template -->") != -1:
+                templaterenderer = importlib.import_module(
+                    "templates."+str(site_url)+".render")
+                return flask.render_template("basic.html", content=flask.Markup(templaterenderer.return_template()), title=title_tag.text.strip())
+            return flask.render_template("basic.html", title=title_tag.text.strip(), content=flask.Markup(content))
+    except Exception as e:
         print(traceback.format_exc())
         return flask.Response(status=404)
-if __name__=="__main__":
-    port = random.randint(1025,65535)
+
+
+if __name__ == "__main__":
+    port = random.randint(1025, 65535)
     print("""!!! WARNING !!! WARNING !!!
 This server is for debugging purposes only.""")
-    app.run(debug=True,port=port,host="127.0.0.1")
+    app.run(debug=True, port=port, host="127.0.0.1")
